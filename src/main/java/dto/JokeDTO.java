b4807719a9275905c621c365cb2c5f8a635d7521
selina <soendengaard@hotmail.com>
2020-09-15 15:42:37 +0200

    added jokeDTO class

---
+++ src/main/java/dto/JokeDTO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import entities.Joke;
+
+/**
+ *
+ * @author soend
+ */
+
+
+public class JokeDTO {
+    private Long id;
+    private String joke;
+    private String topic;
+    
+     
+    public JokeDTO(Joke j) {
+        this.id = j.getId();
+        this.joke = j.getJoke();
+        this.topic = j.getTopic();
+       
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+    
+    
+
+   
+}
