bfebe3bd9e649e14714c9195292227dae4cd8757
kasarama <magdalena.wawrzak@gmail.com>
2020-09-19 02:08:15 +0200

    after clean and build

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -23,6 +23,7 @@ public class ApplicationConfig extends Application {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.AlbumResource.class);
         resources.add(rest.GroupmembersResource.class);
+        resources.add(rest.JokeResource.class);
         resources.add(rest.TestResource.class);
     }
     
